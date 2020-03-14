97318df30303e61c582e6c2db689d07cb5f54dd0
zhujiashuai <jiashuai.zhu@ishansong.com>
2020-03-15 01:17:44 +0800

    最大子序和

---
+++ src/dynamic/MaxSubArray.java
@@ -0,0 +1,30 @@
+package dynamic;
+
+/**
+ * https://leetcode-cn.com/problems/maximum-subarray/
+ */
+public class MaxSubArray {
+
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+
+        int result = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        MaxSubArray array = new MaxSubArray();
+        System.out.println(array.maxSubArray(nums));
+    }
+
+}
