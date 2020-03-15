22ea08d87526a4d6d04ca27526f9e923348ca6b7
zhujiashuai <jiashuai.zhu@ishansong.com>
2020-03-15 15:22:44 +0800

    带环的打家劫舍

---
+++ src/dynamic/HouseRobberII.java
@@ -0,0 +1,42 @@
+package dynamic;
+
+/**
+ * https://leetcode-cn.com/problems/house-robber-ii
+ */
+public class HouseRobberII {
+
+    public int rob(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int[][] dp = new int[nums.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
+        dp[1][0] = nums[1];
+        dp[1][1] = nums[0];
+
+        int result = Math.max(dp[1][0], dp[1][1]);
+        for (int i = 2; i < nums.length; i++) {
+            if ((i + 1) % nums.length == 0) {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][0] + nums[i]);
+            } else {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][0] + nums[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][1] + nums[i]);
+            }
+            result = Math.max(result, dp[i][0] > dp[i][1] ? dp[i][0] : dp[i][1]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 1};
+        HouseRobberII robberII = new HouseRobberII();
+        System.out.println(robberII.rob(nums));
+    }
+}
