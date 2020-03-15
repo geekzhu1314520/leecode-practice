c1eaf2bd2cf0bb483d0af87ca584cd7eb2b7bb5c
zhujiashuai <jiashuai.zhu@ishansong.com>
2020-03-15 17:59:24 +0800

    优化劫富济贫代码

--- src/dynamic/HouseRobberII.java
+++ src/dynamic/HouseRobberII.java
@@ -24,10 +24,8 @@ public class HouseRobberII {
         // dp[i][0]不带第一个节点；dp[i][1]带第一个节点
         int result = Math.max(dp[1][0], dp[1][1]);
         for (int i = 2; i < nums.length; i++) {
-            if ((i + 1) % nums.length == 0) {
-                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][0] + nums[i]);
-            } else {
-                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][0] + nums[i]);
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][0] + nums[i]);
+            if ((i + 1) % nums.length != 0) {
                 dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][1] + nums[i]);
             }
             result = Math.max(result, dp[i][0] > dp[i][1] ? dp[i][0] : dp[i][1]);
